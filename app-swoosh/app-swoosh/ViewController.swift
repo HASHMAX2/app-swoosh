288abfdba60cc67fe345527c2c5981426db8330e
ali hashmi <alihashmi@MacBook-Pro.local>
2020-08-26 12:55:04 +0530

    my 1st commit

---
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  app-swoosh
+//
+//  Created by ali hashmi on 26/08/20.
+//  Copyright © 2020 ali hashmi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
