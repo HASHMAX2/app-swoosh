73fe9f1a0cd6665aae076892ae4c14bb1284d5c3
ali hashmi <alihashmi@MacBook-Pro.local>
2020-08-29 13:35:08 +0530

    my second commit

---
+++ app-swoosh/app-swoosh/Views/BorderButton.swift
@@ -0,0 +1,21 @@
+//
+//  BorderButton.swift
+//  app-swoosh
+//
+//  Created by ali hashmi on 26/08/20.
+//  Copyright © 2020 ali hashmi. All rights reserved.
+//
+
+import UIKit
+
+class BorderButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
